ed9889f6943a193b4aea6535dd574a1439a710e1
GitHub Action <action@github.com>
2021-09-12 18:22:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/berezhkoe/intellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/berezhkoe/intellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.berezhkoe.intellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.berezhkoe.intellijplugin.MyBundle
 
 class MyApplicationService {
 
