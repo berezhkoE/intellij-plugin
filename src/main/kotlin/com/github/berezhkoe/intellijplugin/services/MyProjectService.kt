b0c35cfc5b30b35b830c6f605310aaaef01c8e3b
berezhkoE <berezhko.ei@gmail.com>
2021-12-01 11:50:08 +0300

    tmp2

--- src/main/kotlin/com/github/berezhkoe/intellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/berezhkoe/intellijplugin/services/MyProjectService.kt
@@ -8,6 +8,151 @@ import java.util.function.BinaryOperator
 import java.util.function.IntBinaryOperator
 
 class MyProjectService(project: Project) {
+    var stringRepresentation: String
+        get() = this.toString()
+        set(value) {
+//            if (a               // +1 for `if`
+//
+//                && b && c       // +1
+//                || d || e       // +1
+//                && f            // +1
+//            ) {
+//            }
+        }
+
+
+
+    fun PsiElement.isFunction(a: Boolean, b: Boolean, c: Boolean, d: Boolean, e: Boolean, f: Boolean): Boolean {
+        if (a               // +1 for `if`
+            && b && c       // +1
+            || d || e       // +1
+            && f            // +1
+        ) {
+            return b
+        }
+
+        ppp
+        
+        lll
+        
+
+        if (a               // +1 for `if`
+            &&              // +1
+            !(b && c)       // +1
+        ) {
+            return c
+        }
+        return a
+    }
+
+    fun veryFirst(a: Boolean, b: Boolean, c: Boolean) {
+        val items = listOf(1, 2, 3, 4, 5)
+
+        // Lambdas are code blocks enclosed in curly braces.
+        items.fold(0, {
+            // When a lambda has parameters, they go first, followed by '->'
+                acc: Int, i: Int ->
+            print("acc = $acc, i = $i, ")
+            val result = acc + i
+            if (a == b) {
+                print("")
+            }
+            println("result = $result")
+            // The last expression in a lambda is considered the return value:
+            result
+        })
+    }
+
+    enum class Direction {
+        NORTH, SOUTH, WEST, EAST
+    }
+
+    enum class IntArithmetics : BinaryOperator<Int>, IntBinaryOperator {
+        PLUS {
+            override fun apply(t: Int, u: Int): Int = t + u
+        },
+        TIMES {
+            override fun apply(t: Int, u: Int): Int = t * u
+        };
+
+        override fun applyAsInt(t: Int, u: Int) = apply(t, u)
+    }
+
+
+    interface Inner {
+        private fun testFirst(b: Boolean) {
+            for (i in 1..10) {                  // +1
+                while (true) {                  // +2 (nesting=1)
+                    break                       // +1
+                }
+            }
+
+            for (i in 1..10) {                  // +1
+                while (true) {                  // +2 (nesting=1)
+                    break                       // +1
+                }
+            }
+        }
+    }
+
+    class Nested {
+        companion object {
+            private fun testFirst(b: Boolean) {
+                for (i in 1..10) {                  // +1
+                    while (true) {                  // +2 (nesting=1)
+                        break                       // +1
+                    }
+                }
+
+                for (i in 1..10) {                  // +1
+                    while (true) {                  // +2 (nesting=1)
+                        break                       // +1
+                    }
+                }
+            }
+        }
+
+        private fun testFirst(b: Boolean) {
+            for (i in 1..10) {                  // +1
+                while (true) {                  // +2 (nesting=1)
+                    break                       // +1
+                }
+            }
+
+            for (i in 1..10) {                  // +1
+                while (true) {                  // +2 (nesting=1)
+                    break                       // +1
+                }
+            }
+        }
+    }
+
+    companion object {
+        private fun testFirst(b: Boolean) {
+            for (i in 1..10) {                  // +1
+                while (true) {                  // +2 (nesting=1)
+                    break                       // +1
+                }
+            }
+
+            for (i in 1..10) {                  // +1
+                while (true) {                  // +2 (nesting=1)
+                    break                       // +1
+                }
+            }
+        }
+
+        fun testtest(a: Boolean, b: Boolean, c: Boolean, d: Boolean, e: Boolean, f: Boolean) {
+            a &&                                // +1
+                    !((a || b && c))              // +1
+                    || d || e                   // +1
+                    && f                        // +1
+
+//            a && b && c                         // +1
+//
+//            a && b || c                         // +2
+        }
+    }
 
     init {
         println(MyBundle.message("projectService", project.name))
